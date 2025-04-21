676b979eda808653d3b0272ae94387b6eb29c369
Cristina <cris.correia.santos@gmail.com>
2025-04-21 18:18:53 +0100

    working in Jarvis Care Assistent - VectorStore, AiServiceImpl

---
+++ backend/src/main/java/com/jarviscare/you/services/AiServiceImpl.java
@@ -0,0 +1,61 @@
+package com.jarviscare.you.services;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class AiServiceImpl implements AiService {
+
+    @Value("${ai.rag_prompt_template}")
+    private Resource ragPromptTemplate;
+
+    private ChatClient chatClient;
+    private VectorStore vectorStore;
+
+    @Value("${ai.rag_number_results}")
+    private int numberResults;
+
+    @Override
+    public Generation info(String question) {
+//        List<String> contentList = vectorStore.search(question);
+        List<Document> documents = vectorStore.similaritySearch(SearchRequest.query(question).withTopK(numberResults));
+        List<String> contentList = documents.stream().map(Document::getContent).toList();
+
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+        Prompt prompt = promptTemplate.create(Map.of(
+                "input", question,
+                "documents", String.join("\n", contentList)));
+
+        return chatClient.call(prompt).getResult();
+    }
+
+    /**
+     * Set the chat client
+     * @param chatClient to chat
+     */
+    @Autowired
+    public void setChatClient(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    /**
+     * Set the vector store
+     * @param vectorStore to set
+     */
+    @Autowired
+    public void setStore(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+}
